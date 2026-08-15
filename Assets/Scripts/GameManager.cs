8ef6963984fc61e82ff0d7231cf9a78bcd4418e2
Julaly <mtm296185@gmail.com>
2026-08-15 10:21:02 +0700

    ball ball ball

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Runtime.InteropServices;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
@@ -12,6 +13,15 @@ public class GameManager : MonoBehaviour
 
     [SerializeField]
     private GameObject ballPrefab;
+
+    [SerializeField]
+    private GameObject QueBall;
+
+    [SerializeField]
+    private float xInput = 0f;
+
+    [SerializeField]
+    private GameObject ballLine;
 
     public static GameManager instance;
 
@@ -32,7 +42,27 @@ public class GameManager : MonoBehaviour
 
     void Update()
     {
-        
+        RotateBall();
+
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            ShootBall();
+        }
+        if (Keyboard.current.aKey.wasPressedThisFrame || Keyboard.current.leftArrowKey.isPressed)
+        {
+            xInput = -0.1f;
+        }
+        else if (Keyboard.current.dKey.wasPressedThisFrame || Keyboard.current.rightArrowKey.isPressed)
+        {
+            xInput = 0.1f;
+        }
+        else
+        {
+            xInput = 0f;
+        }
+
+        if (Keyboard.current.backspaceKey.wasPressedThisFrame)
+            StopBall();
     }
 
     private void Setball(BallColor col,int i)
@@ -43,5 +73,27 @@ public class GameManager : MonoBehaviour
 
         Ball b = obj.GetComponent<Ball>();
         b.Color(col);
+    }
+
+    private void ShootBall()
+    {
+        Rigidbody rb = QueBall.GetComponent<Rigidbody>();
+        rb.AddRelativeForce(Vector3.forward * 50, ForceMode.Impulse);
+        ballLine.SetActive(false);
+    }
+
+    private void RotateBall()
+    {
+        if (QueBall != null)
+            QueBall.transform.Rotate(0f, xInput, 0f);
+    }
+
+    private void StopBall()
+    {
+        Rigidbody rb = QueBall.GetComponent<Rigidbody>();
+        rb.linearVelocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+        QueBall.transform.eulerAngles = new Vector3(0f, 0f, 0f);
+        ballLine.SetActive(true);
     }
 }
